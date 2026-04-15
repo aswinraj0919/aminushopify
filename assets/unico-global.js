74951ad880f726f928c3e0408510e1d54e51b198
aswin <aswinraj.stackup@gmail.com>
2026-04-15 10:43:57 +0530

    first commit

---
+++ assets/unico-global.js
@@ -0,0 +1,26 @@
+
+// ----------------------------------------------------
+const carouselWrappers = document.querySelectorAll('.unico-carousel');
+
+carouselWrappers.forEach((carouselWrapper) => {
+    let startX = 0;
+    let endX = 0;
+
+    carouselWrapper.addEventListener('touchstart', (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    carouselWrapper.addEventListener('touchmove', (e) => {
+        endX = e.touches[0].clientX;
+    });
+
+    carouselWrapper.addEventListener('touchend', () => {
+        if (startX > endX + 50) {
+            const nextButton = carouselWrapper.querySelector('[data-carousel-next]');
+            if (nextButton) nextButton.click();
+        } else if (startX < endX - 50) {
+            const prevButton = carouselWrapper.querySelector('[data-carousel-prev]');
+            if (prevButton) prevButton.click();
+        }
+    });
+});
